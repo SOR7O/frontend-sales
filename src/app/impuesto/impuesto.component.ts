db9e60194f7f856de411feef429fe372c56707a5
SOR7O <47393334+SOR7O@users.noreply.github.com>
2024-02-13 13:05:26 -0600

    init impuestos

--- src/app/impuesto/impuesto.component.ts
+++ src/app/impuesto/impuesto.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-impuesto',
@@ -7,6 +10,34 @@ import { Component } from '@angular/core';
   templateUrl: './impuesto.component.html',
   styleUrl: './impuesto.component.css'
 })
-export class ImpuestoComponent {
+export class ImpuestoComponent implements OnInit,OnDestroy{
+  private destroy$: Subject<void> = new Subject<void>();
+  impuestoForm=new FormGroup({
+    _id:new FormControl(''),
+    nombre:new FormControl('',[Validators.required]),
+    correlativo:new FormControl('',[Validators.required]),
+    valor:new FormControl('',[Validators.required]),
+  });
+
+  displayedcolumns= ['nombre','correlativo','valor'];
+  dataSource = new MatTableDataSource();
+
+
+  constructor(private fb:FormBuilder){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  getImpuestos(){
+    // const dataSource = [...response['data']]
+    // this.dataSource = new MatTableDataSource(dataSource);
+  }
+
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
 
 }
